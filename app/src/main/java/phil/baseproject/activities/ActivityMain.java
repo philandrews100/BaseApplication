33eb41750caf0e881968e466d86adb111249ea27
Philip Andrews <phil.andrews.100@gmail.com>
2017-02-01 17:19:33 +0000

    Initial Commit
    - Initial commit of the project

---
+++ app/src/main/java/phil/baseproject/activities/ActivityMain.java
@@ -0,0 +1,15 @@
+package phil.baseproject.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import phil.baseproject.R;
+
+public class ActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
