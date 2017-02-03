69073b034806dbc078b9cf3344d2d78f542fab62
Philip Andrews <phil.andrews.100@gmail.com>
2017-02-03 14:05:25 +0000

    Fragments
    - View container setup

--- app/src/main/java/phil/baseproject/activities/ActivityMain.java
+++ app/src/main/java/phil/baseproject/activities/ActivityMain.java
@@ -6,10 +6,11 @@ import android.os.Bundle;
 import phil.baseproject.R;
 
 public class ActivityMain extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
